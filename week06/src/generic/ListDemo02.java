d07cad0d409406bba53d15881be791cddbad3431
xksdn <xksdn7623@gmail.com>
2023-10-04 15:02:31 +0900

    week06 v31

--- week06/src/generic/ListDemo02.java
+++ week06/src/generic/ListDemo02.java
@@ -9,12 +9,18 @@ import java.util.List;  // built in
 public class ListDemo02 {
     public static void main(String[] args) {
         List<String> skills = new ArrayList<>();
+        List<Integer> sqecialAttackRate = new ArrayList<>();
+
         skills.add("화염방사");
         skills.add("용의 숨결");
         skills.add("할퀴기");
+        sqecialAttackRate.add(90);
+        sqecialAttackRate.add(60);
+        sqecialAttackRate.add(40);
 
         for(String skill : skills)
             System.out.println(skill);
-
+        for (Integer Rate : sqecialAttackRate)
+            System.out.println(Rate);
     }
 }
