210a06082db6d1c9cef50e84c1f81e25ada51778
xksdn <xksdn7623@gmail.com>
2023-10-18 14:31:32 +0900

    week08 v42

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -71,9 +71,12 @@ public class PokemonGame {
         } catch (InputMismatchException err) {
             System.out.println("입력 값은 숫자입니다.");
             System.out.println("예외 내용 : " + err.getMessage());
-        }catch (NullPointerException err) {
+        } catch (NullPointerException err) {
             System.out.println("플레이어 객체가 생성되지 않았습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
+        } catch (IndexOutOfBoundsException err) {
+            System.out.println("존재하지 않는 스킬입니다. 기술 범위를 벗어남");
+            System.out.println("예외 내용 : " + err.getMessage());;
         } catch (Exception err) {
             System.out.println("예외가 발생했습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
