71e5cbd0c9cd97f3015eda7d2dac6a65fb0a0b43
xksdn <xksdn7623@gmail.com>
2023-11-22 15:24:24 +0900

    week1213 v54

--- week1213/src/Pokemonster/Pokemon.java
+++ week1213/src/Pokemonster/Pokemon.java
@@ -8,17 +8,19 @@ import java.util.Map;
 //public final class Pokemonster.Pokemon {
 public abstract class Pokemon {
     //private int level;
-    protected int level;
     private int hp;
+    protected int level;
     protected String name;
     protected int attackRate;
     protected int defenceRate;
-//    protected String [] skills;
-//    protected int [] specialAttackRate;
+
+//    protected String[] skills;
+//    protected int[] specialAttackRate;
 //    protected List<String> skills;
 //    protected List<Integer> specialAttackRate;
 
-    protected Map<String, Integer> skills;
+    protected Map<Integer, String> skills;
+    protected Map<String, Integer> specialAttackRate;
     private static int pokemonCount = 0;  // 클래스(정적) 변수
 
     Flyable flyable;  // 연관 관계
@@ -65,7 +67,6 @@ public abstract class Pokemon {
         this.hp = hp;
     }
 
-
     public void evolve(){  // 매개변수 제거
         if(this instanceof Pikachu){
             System.out.println("삐까삐까~");
@@ -94,19 +95,17 @@ public abstract class Pokemon {
     }
 
     public abstract void attack();
-//    public void attack(Pokemon targetPokemon, int skillnumber){
-    public void attack(Pokemon targetPokemon, String skill){
-        System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 " + skill +  " 공격 시전!");
-        int temporaryAttackRate = (this.attackRate + this.skills.get(skill)) - targetPokemon.defenceRate;
-        if (temporaryAttackRate < 0)
+
+    public void attack(Pokemon targetPokemon, int skillNumber){
+        System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ this.skills.get(skillNumber) +" 공격 시전!");
+        int temporaryAttackRate = (this.attackRate + this.specialAttackRate.get(this.skills.get(skillNumber))) - targetPokemon.defenceRate;
+        if(temporaryAttackRate < 0)
             temporaryAttackRate = 0;
         targetPokemon.hp = targetPokemon.hp - temporaryAttackRate;
-
-        if(targetPokemon.hp <= 0) {
+        if(targetPokemon.hp <= 0){
             System.out.println(targetPokemon.name + "은(는) 사망!");
-        } else {
-            System.out.println(targetPokemon.name + "의 채력은 " + targetPokemon.hp + "입니다.");
+        }else{
+            System.out.println(targetPokemon.name + "의 체력은 " + targetPokemon.hp + "입니다");
         }
     }
-
 }
